8ea502028569354626960009e60f82205c1eab18
thomas.spigel@gmail.com <tspigel@comwrap.com>
2014-02-01 01:29:36 +0100

    Put a override to check if it's admin and put the correct design path

--- magento1Modules/WizardsFugue/Core/Model/Design/Package.php
+++ magento1Modules/WizardsFugue/Core/Model/Design/Package.php
@@ -5,21 +5,26 @@
 
 class WizardsFugue_Core_Model_Design_Package extends Mage_Core_Model_Design_Package
 {
-    
+
     protected function _fallback($file, array &$params, array $fallbackScheme = array(array()))
     {
-        $result = parent::_fallback($file, $params, $fallbackScheme);
-        /*
-        var_dump(
-            $file,
-            $result,
-            file_exists($result)
-        );
-        die(__FILE__.':'.__LINE__);
-         */
+	    if($params['_area'] == 'adminhtml') {
+		    // set on the custom admin design dir
+		    Mage::app()->getConfig()->getOptions()->setData('design_dir', Mage::app()->getConfig()->getOptions()->getAdminDesignDir());
+	    }
+
+	    $result = parent::_fallback($file, $params, $fallbackScheme);
+
+
         if( !$result ){
-            $result = 'frontend/base/default/template/' . $file;
+	        if($params['_area'] == 'adminhtml') {
+		        $result = 'adminhtml/default/default/' . $file;
+	        } else {
+		        $result = 'frontend/base/default/template/' . $file;
+
+	        }
         }
         return $result;
     }
+
 }
