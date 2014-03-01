894f82ea6db731f3d38f1597fff068081d9891ce
Flyingmana <flyingmana@googlemail.com>
2014-03-01 16:37:44 +0100

    prepare further changes on config model

--- src/WizardsFugue/Magenta1/Core/Model/Config.php
+++ src/WizardsFugue/Magenta1/Core/Model/Config.php
@@ -15,6 +15,7 @@ class Config extends \Mage_Core_Model_Config{
     public function __construct($sourceData=null)
     {
         parent::__construct($sourceData);
+        $this->_options         = new Config\Options($sourceData);
         if( $sourceData && isset( $sourceData['wizards_fudge']['paths'] ) )
         {
             foreach( $sourceData['wizards_fudge']['paths'] as $key=>$val ){
---
+++ src/WizardsFugue/Magenta1/Core/Model/Config/Options.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+
+namespace WizardsFugue\Magenta1\Core\Model\Config;
+
+
+class Options extends \Mage_Core_Model_Config_Options{
+
+
+    
+    
+} 
